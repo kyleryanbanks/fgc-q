486a6c4d7263dbe900b4d35b6004fea41a899b5c
Kyle Banks <kyle.banks@synapsewireless.com>
2018-09-13 21:40:31 -0500

    timer working unstyled

--- src/app/scoreboard/components/timer/timer.component.ts
+++ src/app/scoreboard/components/timer/timer.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { DateTime, Duration } from "luxon";
+import { interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'fgcq-timer',
+  selector: "fgcq-timer",
   template: `
     <p>
-      timer works!
+      {{runtime.hours | number:'2.0-0'}} : {{runtime.minutes | number:'2.0-0'}} : {{runtime.seconds | number:'2.0-0'}}
     </p>
+    <button (click)="resetTimer()">Reset Timer</button>
   `,
   styles: []
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, OnDestroy {
+  startOfMatch: DateTime;
+  runtime: Duration;
+  timerSub: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.startOfMatch = DateTime.local();
+    this.runtime = Duration.fromMillis(0);
+
+    this.timerSub = interval(1000).subscribe(() => {
+      this.runtime = this.startOfMatch
+        .diffNow(["hours", "minutes", "seconds", "milliseconds"])
+        .negate();
+    });
   }
 
+  ngOnDestroy() {
+    this.timerSub.unsubscribe();
+  }
+
+  resetTimer() {
+    this.startOfMatch = DateTime.local();
+  }
 }
